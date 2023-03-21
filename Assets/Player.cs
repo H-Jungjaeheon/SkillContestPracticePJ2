496f0d9ee05e1e6b9a10662a88a4661b893b1d40
H-JungJaeHeon <jjhking222@naver.com>
2023-03-21 20:57:26 +0900

    테스트2
    테스트2

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField]
+    private float speed;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Move());
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        //horizontal = Input.GetAxisRaw("Horizontal");
+        //vertical = Input.GetAxisRaw("Vertical");
+
+        //transform.Translate(moveVector * Time.deltaTime);
+
+        //transform.position = speed * Time.deltaTime * horizontal;
+    }
+
+    IEnumerator Move()
+    {
+        Vector3 moveVector = Vector3.zero;
+
+        string horizontal = "Horizontal";
+        string vertical = "Vertical";
+
+        while (true)
+        {
+            if (transform.position.x + (Input.GetAxisRaw(horizontal) * speed * Time.deltaTime) < 5f && transform.position.x + (Input.GetAxisRaw(horizontal) * speed * Time.deltaTime) > -5f)
+            {
+                moveVector.x = Input.GetAxisRaw(horizontal);
+            }
+            else
+            {
+                moveVector.x = 0f;
+            }
+
+            moveVector.z = Input.GetAxisRaw(vertical);
+
+            transform.Translate(moveVector * Time.deltaTime * speed);
+
+            yield return null;
+        }
     }
 }
